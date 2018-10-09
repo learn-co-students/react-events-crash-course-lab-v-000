1198807cc3fec717d125b31a70a8a4dfed91cacd
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-10-09 00:29:43 +0000

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -3,7 +3,7 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
-  
+
   //handleMouseMove = (event) => {
     /* TODO: This method should capture the `x` and `y` coordinates of the mouse
      * from the event and use them to invoke the `drawChromeBoiAtCoords`
@@ -11,24 +11,32 @@ export default class ChromeBoisDomain extends Component {
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
   //}
-  
+
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
-   
+   handleOnClick = (event) => {
+    toggleCycling()
+  /* TODO: Create an event handler which, when fired, invokes the provided
+   * `toggleCycling` function with no arguments. Don't forget the click event
+   * listener that should fire it!
+   */
+  }
+
+
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the the
   /* provided `resize` function with a single argument of either '+' or '-'
   /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-' 
+  /* if the key pressed was 's', then it should call `resize` with '-'
    */
 
-  
+
   handleMouseMove = (event) => {
     drawChromeBoiAtCoords(event.clientX, event.clientY)
   }
-  
+
   handleKeyDown = (event) => {
     if (event.key === 'a') {
       resize('+')
@@ -36,10 +44,10 @@ export default class ChromeBoisDomain extends Component {
       resize('-')
     }
   }
-   
+
   render() {
     return (
-      <canvas 
+      <canvas
         onMouseMove={this.handleMouseMove}
         onClick={() => {toggleCycling()}}
         onKeyDown={this.handleKeyDown}
