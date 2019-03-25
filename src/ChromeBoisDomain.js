8cc1146511bd277d3ed8f815b63648a6bf4a2308
arianaberger <ariana.berger@gmail.com>
2019-03-25 18:56:31 -0400

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -4,19 +4,35 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 export default class ChromeBoisDomain extends Component {
   
+
+/* TODO: This method should capture the `x` and `y` coordinates of the mouse
+* from the event and use them to invoke the `drawChromeBoiAtCoords`
+* function that has been provided and is already imported
+* (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
+*/
+
   handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
-     * from the event and use them to invoke the `drawChromeBoiAtCoords`
-     * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
-     */
-  }
-  
+    // document.addEventListener('mousemove', function (e) => {
+      // let clientX = e.screenX;
+      // let clientY = e.screenY;
+    let x = event['clientX'];
+    let y = event['clientY']
+    drawChromeBoiAtCoords(x, y);
+  };
+
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
    
+    handleKeyDown = function(event) {
+      if (event.which === 65) {
+        resize('+');
+      } else if (event.which === 83) {
+        resize('-');
+      }
+    };
+
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the the
   /* provided `resize` function with a single argument of either '+' or '-'
@@ -27,6 +43,9 @@ export default class ChromeBoisDomain extends Component {
   render() {
     return (
       <canvas 
+        // onClick={function() {toggleCycling()}}
+        onClick = {() => {toggleCycling()}}
+        onKeyDown={this.handleKeyDown}
         onMouseMove={this.handleMouseMove}
         width='900'
         height='600'
