574be48d99d64a7cb30e1f5c3c5427ada03fe86a
github username <github email address>
2019-05-25 17:02:41 -0500

    minor changes still not passing

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -6,21 +6,30 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 export default class ChromeBoisDomain extends Component {
   
   handleMouseMove = (event) => {
-    drawChromeBoiAtCoords(event.clientX, event.clientY)
+    // let x = event.clientX;
+    // let y = event.clientY;
+
+    drawChromeBoiAtCoords(event.clientX, event.clientY);
   }
     
+  toggleCycling = (event) => {
+
+    }
+
   handleKeyDown = (event) => {
     if(event.key === 'a') {
       resize('+')
     } else if (event.key === 's') {
       resize('-')
     }
-    }  
+  }  
    
   render() {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={toggleCycling()}
+        onKeyDown={this.handleKeyDown}
         width='900'
         height='600'
         tabIndex="0">
